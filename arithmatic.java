a6ab75ccc4cd061730200a2ba2002c8bf02a6f05
PriyaPatil248 <114169890+PriyaPatil248@users.noreply.github.com>
2022-10-05 16:12:08 +0530

    JAVA Programs

---
+++ arithmatic.java
@@ -0,0 +1,11 @@
+class arithmatic
+{
+ public static void main(String arg[])
+  {
+     int a=50, b=5;
+     System.out.println("Addition is "+(a+b));
+     System.out.println("Substraction is "+(a-b));
+     System.out.println("Multipication is "+(a*b));
+     System.out.println("Division is "+(a/b));
+   }
+}
---
+++ string.java
@@ -0,0 +1,11 @@
+class string
+{
+  public static void main(String arg [])
+  {
+    String s[]={"Teju","Naaz","Dipu","Priya","Swa"};
+    for(String b:s)
+    {
+       System.out.println(b);
+    }
+  }
+}
