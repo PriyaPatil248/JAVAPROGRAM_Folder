59a99e476d4880bdce294105f2be41dfe9dcdf87
PriyaPatil248 <114169890+PriyaPatil248@users.noreply.github.com>
2022-10-31 09:16:13 +0530

    Program on Matrix

---
+++ matrix.java
@@ -0,0 +1,26 @@
+public class matrix
+{
+   public static void main(String args[])
+   {
+      int a[][]={{1,1,1},{4,2,2},{3,5,3}};
+     int b[][]={{1,1,1},{2,6,2},{3,1,3}};
+     int c[][]=new int[3][3];
+    System.out.println("Product of given two matrices is:");
+
+    for(int i=0;i<3;i++)
+   {
+   for(int j=0;j<3;j++)
+   {
+     c[i][j]=0;
+   for(int k=0;k<3;k++)
+   {
+    c[i][j]+=a[i][k]*b[k][j];
+    }
+
+   System.out.print(c[i][j]+" ");
+
+    }
+   System.out.println();
+  }
+ }
+}
